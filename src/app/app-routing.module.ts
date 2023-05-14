e816d405d8bbbea9726afc139d32e22d43837f3f
VICTOR AMWOLLO <amwollo@yahoo.com>
2023-05-14 12:27:24 +0300

    add view-supermarket route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ViewSupermarketComponent } from './view-supermarket/view-supermarket.component';
 
 const routes: Routes = [
   {path:  "", pathMatch:  "full",redirectTo:  "home"},
-  {path: "home", component: HomeComponent}
+  {path: "home", component: HomeComponent},
+  {path: "view-supermarket/:id", component: ViewSupermarketComponent},
 ];
 
 @NgModule({
