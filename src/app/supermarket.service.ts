5336c597503727bfdd01dfaf36b8c275b3f11782
VICTOR AMWOLLO <amwollo@yahoo.com>
2023-05-14 18:44:12 +0300

    fetch data from remote server

--- src/app/supermarket.service.ts
+++ src/app/supermarket.service.ts
@@ -1,46 +1,80 @@
 import { Injectable } from '@angular/core';
 import { SupermarketModel } from './models/supermarket-model.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { throwError,Observable, catchError, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupermarketService {
   supermarkets:SupermarketModel[] = [
-    new SupermarketModel(1,"Supermarket 1","Location 1"),
-    new SupermarketModel(2,"Supermarket 2","Location 2"),
-    new SupermarketModel(3,"Supermarket 3","Location 3"),
-    new SupermarketModel(4,"Supermarket 4","Location 4"),
-    new SupermarketModel(5,"Supermarket 5","Location 5"),
-    new SupermarketModel(6,"Supermarket 6","Location 6"),
-    new SupermarketModel(7,"Supermarket 7","Location 7"),
-    new SupermarketModel(8,"Supermarket 8","Location 8"),
+    // new SupermarketModel(1,"Supermarket 1","Location 1"),
+    // new SupermarketModel(2,"Supermarket 2","Location 2"),
+    // new SupermarketModel(3,"Supermarket 3","Location 3"),
+    // new SupermarketModel(4,"Supermarket 4","Location 4"),
+    // new SupermarketModel(5,"Supermarket 5","Location 5"),
+    // new SupermarketModel(6,"Supermarket 6","Location 6"),
+    // new SupermarketModel(7,"Supermarket 7","Location 7"),
+    // new SupermarketModel(8,"Supermarket 8","Location 8"),
   ];
 
-  constructor() { 
+  baseUrl:string = "http://localhost:8000/api";
+
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  constructor(private httpClient : HttpClient) { 
   }
 
-  public getSupermarkets():Array<SupermarketModel>{
-    return this.supermarkets;
+  public getSupermarkets(){
+    return this.httpClient.get(this.baseUrl + '/list-supermarkets');
   }
 
-  public getSupermarketById(supermarketId:number):SupermarketModel {
-    return this.supermarkets.find(item=>item.id === supermarketId);
+  public getSupermarketById(supermarketId:string):Observable<SupermarketModel> {
+    return this.httpClient.get<SupermarketModel>(this.baseUrl + '/view-supermarket/'+supermarketId)
+    .pipe(retry(1), catchError(this.handleError));
   }
 
-  public deleteSupermarketById(supermarketId:number):boolean {
-    var itemIndex =  this.supermarkets.findIndex(item=>item.id === supermarketId);
-    if (itemIndex > -1) {
-      this.supermarkets.splice(itemIndex,1);
-      console.log("supermarkets")
-      console.log(supermarketId)
-      console.log(itemIndex)
+  // HttpClient API delete() method => Delete supermarket
+  public deleteSupermarketById(supermarketId: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    return this.httpClient.delete<SupermarketModel>(this.baseUrl + '/delete-supermarket/'+supermarketId, httpOptions)
+      .pipe(retry(1), catchError(this.handleError));
+  }
 
-      console.log(this.supermarkets)
-      return true;
-    } else {
-      return false;
+  // HttpClient API put() method => Update supermarket
+  public updateSupermarket(supermarketId: any,supermarket:any): Observable<SupermarketModel> {
+    return this.httpClient
+      .post<SupermarketModel>(
+        this.baseUrl + '/update-supermarket/'+supermarketId,
+        JSON.stringify(supermarket),
+        this.httpOptions
+      )
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+    // Error handling
+    public handleError(error: any) {
+      let errorMessage = '';
+      if (error.error instanceof ErrorEvent) {
+        // Get client-side error
+        errorMessage = error.error.message;
+      } else {
+        // Get server-side error
+        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      }
+      window.alert(errorMessage);
+      return throwError(() => {
+        return errorMessage;
+      });
     }
-
-  }
 
 }
