da4d4d754d7fcdc190cfd031ac4e5074b78e7500
VICTOR AMWOLLO <amwollo@yahoo.com>
2023-05-14 10:46:18 +0300

    Add new components to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddSupermarketComponent } from './add-supermarket/add-supermarket.component';
+import { ViewSupermarketComponent } from './view-supermarket/view-supermarket.component';
+import { EditSupermarketComponent } from './edit-supermarket/edit-supermarket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddSupermarketComponent,
+    ViewSupermarketComponent,
+    EditSupermarketComponent
   ],
   imports: [
     BrowserModule,
