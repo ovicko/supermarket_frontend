5f31918fead2dc530dd4da66e76eb1e52925a5a8
VICTOR AMWOLLO <amwollo@yahoo.com>
2023-05-14 12:43:41 +0300

    add dynamic view file

--- src/app/view-supermarket/view-supermarket.component.ts
+++ src/app/view-supermarket/view-supermarket.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SupermarketModel } from '../models/supermarket-model.model';
+import { SupermarketService } from '../supermarket.service';
 
 @Component({
   selector: 'app-view-supermarket',
@@ -6,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-supermarket.component.css']
 })
 export class ViewSupermarketComponent {
+  supermarket:SupermarketModel;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,private service:SupermarketService  ) {}
+
+    ngOnInit() {
+      const supermarketId = Number(this.route.snapshot.paramMap.get('id'));
+      this.supermarket = this.service.getSupermarketById(supermarketId);
+    }
 
 }
