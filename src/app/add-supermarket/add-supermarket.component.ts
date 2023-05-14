2b658d2da2b3a1f1ad05737772f5889e242f0350
VICTOR AMWOLLO <amwollo@yahoo.com>
2023-05-14 18:42:18 +0300

    add supermarket component

--- src/app/add-supermarket/add-supermarket.component.ts
+++ src/app/add-supermarket/add-supermarket.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
+import { SupermarketService } from '../supermarket.service';
+import { SupermarketModel } from '../models/supermarket-model.model';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-add-supermarket',
   templateUrl: './add-supermarket.component.html',
   styleUrls: ['./add-supermarket.component.css']
 })
-export class AddSupermarketComponent {
+export class AddSupermarketComponent implements OnInit{
 
+  supermarketForm:FormGroup;
+
+  name: FormControl;
+  location: FormControl;
+
+  constructor(public dataService: SupermarketService,private serverClient:HttpClient,private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createForm() {
+    this.supermarketForm = new FormGroup({
+      name: this.name,
+      location: this.location
+    });
+  }
+
+  createFormControls() {
+    this.name = new FormControl("", Validators.required);
+    this.location = new FormControl("", Validators.required);
+  }
+
+  onSubmit() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: 'my-auth-token'
+      })
+    };
+    if (this.supermarketForm.valid) {
+      console.log("Form Submitted!");
+      console.log(this.supermarketForm.value);
+      
+      // this.supermarketForm.reset();
+
+      this.serverClient.post<SupermarketModel>(
+        "http://127.0.0.1:8000/api/add-supermarkets",
+        JSON.stringify(this.supermarketForm.value), httpOptions)
+        .subscribe({
+          next: (response) => this.router.navigate(['/']),
+          error: (error) => console.log(error),
+      });
+    }
+  }
+
+  private handleError(error: any):Promise<any> {
+    return Promise.reject(error.message || error );
+  }
 }
